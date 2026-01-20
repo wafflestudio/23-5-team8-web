0c1efcb977ef9c6f57b2f5fa98eab06af18aeb95
오세민 <osemin@osemin-ui-MacBookPro.local>
2026-01-20 15:31:30 +0900

    검색 로직 최적화 및 알림창 개선

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -1,22 +1,32 @@
-import {api} from './axios';
+import { api } from "./axios";
 import type {
   PreEnrollAddRequest,
   PreEnrollCourseResponse,
   PreEnrollUpdateCartCountRequest,
-} from '../types/apiTypes.tsx';
+} from "../types/apiTypes.tsx";
 
 // 장바구니 조회 API
-export const getPreEnrollsApi = async (overQuotaOnly = false) => {
-  return await api.get<PreEnrollCourseResponse[]>('/api/pre-enrolls', {
-    params: {
-      overQuotaOnly,
+export const getPreEnrollsApi = async (
+  overQuotaOnly = false,
+) => {
+  return await api.get<PreEnrollCourseResponse[]>(
+    "/api/pre-enrolls",
+    {
+      params: {
+        overQuotaOnly,
+      },
     },
-  });
+  );
 };
 
 // 장바구니에 강의 추가 API
-export const addPreEnrollApi = async (data: PreEnrollAddRequest) => {
-  return await api.post<PreEnrollCourseResponse>('/api/pre-enrolls', data);
+export const addPreEnrollApi = async (
+  data: PreEnrollAddRequest,
+) => {
+  return await api.post<PreEnrollCourseResponse>(
+    "/api/pre-enrolls",
+    data,
+  );
 };
 
 // 장바구니 항목 cartCount 수정 API
@@ -31,6 +41,10 @@ export const updateCartCountApi = async (
 };
 
 // 장바구니에서 강의 삭제 API
-export const deletePreEnrollApi = async (courseId: number) => {
-  return await api.delete(`/api/pre-enrolls/${courseId}`);
+export const deletePreEnrollApi = async (
+  courseId: number,
+) => {
+  return await api.delete(
+    `/api/pre-enrolls/${courseId}`,
+  );
 };
